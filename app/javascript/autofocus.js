493416b7fe3dadbce5b2337682456f61c4d8267f
laszloscheers <laszloscheers.gmail.com>
2023-06-27 14:42:16 +0200

    input field focus behaviour added

--- app/javascript/autofocus.js
+++ app/javascript/autofocus.js
@@ -1,22 +1,62 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Obtén el último ID de input almacenado en el localStorage (si existe)
-    const lastInputId = localStorage.getItem("lastInputId");
+// Gets all the tasks buttons to add an event listener
+const taskButtons = document.getElementsByClassName("add-task-button");
 
-    // Si hay un último ID de input almacenado, enfoca en ese input
-    if (lastInputId) {
-        const lastInput = document.getElementById(lastInputId);
-        if (lastInput) {
-        lastInput.focus();
-        }
-    }
+// Transforms the html collection into an array
+var taskButtonsArray = Array.from(taskButtons);
 
-    // Agrega un event listener para los eventos de submit en los formularios
-    document.addEventListener("submit", function(event) {
-        // Obtén el formulario y el input donde se hizo submit
-        const form = event.target;
-        const input = form.querySelector("input[type='text']");
+// Loops through the array to add event listeners to echa button, this will keep the id of the last iput field ID for the "Add task form"
+taskButtonsArray.forEach((button) => {
+    const bottonId = button.id
+    const DOMButton = document.getElementById(bottonId);
+    
+    DOMButton.addEventListener("click", function(event) {
+        // 
+        const input = document.getElementById("text-field" + bottonId);
 
         // Guarda el ID del último input en el localStorage
         localStorage.setItem("lastInputId", input.id);
     });
 });
+
+// Gets all the delete list buttons to add an event listener
+const deleteListButtons = document.getElementsByClassName("delete-list-button");
+console.log(deleteListButtons)
+// Transforms the html collection into an array
+var deleteListButtonsArray = Array.from(deleteListButtons);
+
+// Loops through the array to add event listeners to each button, this will errase the last submited input field id to restore local storage
+deleteListButtonsArray.forEach((button) => {
+    const bottonId = button.id
+    const DOMButton = document.getElementById(bottonId);
+    console.log(DOMButton)
+    DOMButton.addEventListener("click", function(event) {
+
+        // Guarda el ID del último input en el localStorage
+        localStorage.removeItem("lastInputId");
+    });
+});
+
+// Gets the last input field ID that was used to add a task if any
+const lastInputId = localStorage.getItem("lastInputId");
+
+// Gets the first input field ID if any
+const firstInputId = taskButtonsArray[0].id
+console.log(firstInputId)
+
+// Set the focus to that input field
+if (lastInputId) {
+
+    const lastInput = document.getElementById(lastInputId);
+
+    if (lastInput) {
+        lastInput.focus();
+    }
+
+} else if (firstInputId){
+
+    const firstInput = document.getElementById("text-field" + firstInputId);
+
+    if (firstInput) {
+        firstInput.focus();
+    }
+}
